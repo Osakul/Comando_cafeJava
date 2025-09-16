4c91e0f5a15de71071134649c6b56b8508913ebe
Osakul <79723900+Osakul@users.noreply.github.com>
2025-09-16 10:28:31 -0300

    Atualização da Atv ED_1

--- Atv01_ED_Arrays/src/IdadeMaiorque18.java
+++ Atv01_ED_Arrays/src/IdadeMaiorque18.java
@@ -1,3 +1,5 @@
+/* 1 Criar um vetor com 10 idades e exibir em tela as idades maiores que 18 anos */
+
 public class IdadeMaiorque18 {
     public static void main(String[] args) throws Exception {
         int[] idades = {12, 19, 22, 8, 18, 30, 15, 21, 40, 17};
--- Atv01_ED_Arrays/src/MediaNotas.java
+++ Atv01_ED_Arrays/src/MediaNotas.java
@@ -1,3 +1,5 @@
+/* 2 Criar um vetor com 4 notas e exibir em tela a média aritmética das notas */
+
 public class MediaNotas {
     public static void main(String[] args) {
         double[] notas = {7.5, 8.0, 6.0, 9.5};
